a17e176562eaea34693a8e9477d0d2bda73e1d76
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-04 21:57:28 -0300

    criacao do model pessoa

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200)
+    biografia = models.TextField()
+    url_foto = models.URLField(max_length=1000)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_modificacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
