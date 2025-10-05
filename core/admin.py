f45040af94db99dc7a6c8aa3eae1eff267151527
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-04 22:14:55 -0300

    feat: customizacoes e personalizacoes na area administrativa

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_criacao', 'data_modificacao') # Define os campos a serem exibidos na lista
+    search_fields = ('nome',) #  Adiciona uma barra de pesquisa para o campo 'nome'
+
 # Register your models here.
-admin.site.register(Pessoa)
+admin.site.register(Pessoa, PessoaAdmin)
