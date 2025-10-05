cdf0783387f8b2a92fb398b4281889f133cf76b5
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-05 11:47:32 -0300

    feat: definir a view de lista na aplicacao #1

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def lista(request):
+    return render(request, 'core/lista.html')
