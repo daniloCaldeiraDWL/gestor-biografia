caf6f6ef938c63bdb984a76ccd9bcf180c9551b9
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-05 12:26:52 -0300

    feat: inclusao do context pessoa

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
 
 def lista(request):
-    return render(request, 'core/lista.html')
+    pessoas = Pessoa.objects.all()
+    context = {
+        'pessoas': pessoas
+    }
+    return render(request, 'core/lista.html', context=context)
