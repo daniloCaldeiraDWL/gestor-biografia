ecce7e85446a20ff4aab0cc99d2e5f2864d7b719
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-05 13:16:32 -0300

    feat: trabalhando com arquivos estaticos no projeto #1

--- biografia/settings.py
+++ biografia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,10 @@ USE_TZ = False # Para não ter problemas com data e hora no SQLite
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
